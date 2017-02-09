9a7fbf6c35cf8325fea2b3c265326fdbc756f29a
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2017-02-10 01:13:56 +0800

    bump the version

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -147,7 +147,7 @@ if (_.any([localStorage.install_time, localStorage.version], _.isUndefined)) {
 }
 
 _.assign(localStorage, {
-  version: '0.1.1',
+  version: '0.1.2',
   install_time: _.now(),
   theme: 'light',
   width: 600,

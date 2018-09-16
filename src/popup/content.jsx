073639ffd6677a3242fad9eab896e6d86b30943a
Arianrhod <theguidanceofawhitetower@gmail.com>
2018-09-16 22:39:43 +0800

    add content classnames

--- src/popup/content.jsx
+++ src/popup/content.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import browser from 'webextension-polyfill'
 import url from 'url'
+import classnames from 'classnames'
 
 class App extends Component {
   constructor (props) {
@@ -51,6 +52,54 @@ class App extends Component {
     }
   }
 
+  getCategoryPageClass (category) {
+    const categoryName = category.split('~')[0]
+    switch (categoryName) {
+      case 'ansible':
+      case 'bottle':
+      case 'codeigniter':
+      case 'django':
+      case 'falcon':
+      case 'matplotlib':
+      case 'numpy':
+      case 'pandas':
+      case 'python':
+      case 'scikit_image':
+      case 'scikit_learn':
+      case 'statsmodels':
+      case 'twig':
+        return 'sphinx'
+      case 'babel':
+      case 'bluebird':
+      case 'eslint':
+      case 'homebrew':
+      case 'jsdoc':
+      case 'react':
+      case 'relay':
+        return 'simple'
+      case 'backbone':
+        return 'underscore'
+      case 'chef':
+      case 'cmake':
+      case 'godot':
+      case 'julia':
+      case 'opentsdb':
+        return 'sphinx_simple'
+      case 'cpp':
+        return 'c'
+      case 'padrino':
+        return 'rubydoc'
+      case 'phoenix':
+        return 'elixir'
+      case 'sass':
+        return 'yard'
+      case 'symfony':
+        return 'laravel'
+      default:
+        return categoryName
+    }
+  }
+
   render () {
     const {pending} = this.state
     if (pending) {
@@ -66,11 +115,16 @@ class App extends Component {
     }
 
     const {content} = this.state
+    let category = this.props.location.pathname.split(/\/|~/g)
+    if (category) {
+      category = category[1] || ''
+    }
     return (
       <div className="_container" role="document">
         <div className="_content" role="main">
           <div
-            className="_page _{getCategoryPageClass(contentEntry.category)}"
+            className="_page"
+            className={classnames(['_page', `_${this.getCategoryPageClass(category)}`])}
             onClick={this.onContentClick}
             dangerouslySetInnerHTML={{__html: content}}>
           </div>

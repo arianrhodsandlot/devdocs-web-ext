6ed0d3c8e1ed79ac6d61fb6ffb06749774808a4d
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2019-03-03 22:55:47 +0800

    finish new options ui styles

--- src/options/app.jsx
+++ src/options/app.jsx
@@ -1,54 +1,76 @@
 import React from 'react'
-import {Slider, Typography, Radio, Checkbox, Button, Grid, GridCell, Elevation} from 'rmwc/index.tsx'
+import i18n from './i18n'
+import {Slider, Typography, Radio, FormField, Button, Elevation} from 'rmwc/index.tsx'
 
 const App = () => (
   <>
-    <Typography use="subtitle2" tag="h2">SIZE</Typography>
-    <Typography use="subtitle2" tag="h3">WIDTH</Typography>
-    <Slider
-      discrete
-      displayMarkers
-      min={100}
-      max={200}
-      step={5}
-    />
-    <Typography use="subtitle2" tag="h3">HEIGHT</Typography>
-    <Slider
-      discrete
-      displayMarkers
-      min={100}
-      max={200}
-      step={5}
-    />
+    <Typography use="subtitle2" tag="h2">{i18n('optionsWindowSize')}</Typography>
+    <Elevation className='elevation-with-padding'>
+      <Elevation>
+        <Typography use="subtitle2" tag="h3">{i18n('optionsWidth')}</Typography>
+        <Slider
+          className='slider-size slider-width'
+          discrete
+          displayMarkers
+          min={300}
+          max={800}
+          step={50}
+        />
+      </Elevation>
+      <Elevation>
+      <Typography use="subtitle2" tag="h3">{i18n('optionsHeight')}</Typography>
+      <Slider
+        className='slider-size slider-height'
+        discrete
+        displayMarkers
+        min={300}
+        max={600}
+        step={50}
+      />
+      </Elevation>
+    </Elevation>
 
-    <Typography use="subtitle2" tag="h3">theme</Typography>
-    <Radio
-      value="cookies">
-      Cookies
-    </Radio>
-    <Radio
-      value="pizza">
-      Pizza
-    </Radio>
+    <Typography use="subtitle2" tag="h3">{i18n('optionsTheme')}</Typography>
+    <Elevation>
+      <Radio
+        name='theme'
+        value="light">
+        {i18n('optionsLight')}
+      </Radio>
+      <Radio
+        name='theme'
+        value="dark">
+        {i18n('optionsDark')}
+      </Radio>
+    </Elevation>
 
-    <Typography use="subtitle2" tag="h3">DOCS</Typography>
-    <Checkbox label="Pizza" />
+    <Typography use="subtitle2" tag="h3">{i18n('optionsDocs')}</Typography>
+    <Elevation className='elevation-with-padding'>
+      <Button icon="open_in_new" outlined dense href='xxx' tag='a'>{i18n('optionsSelectFrom')}</Button>
+    </Elevation>
 
-    <Typography use="subtitle2" tag="h3">other</Typography>
-    <Button icon="keyboard" outlined dense>hotkey</Button>
+    <Typography use="subtitle2" tag="h3">{i18n('optionsShortcut')}</Typography>
+    <Elevation className='elevation-with-padding'>
+      <Button icon="keyboard" outlined dense>{i18n('optionsConfigureShortcuts')}</Button>
+    </Elevation>
 
-    <Elevation style={{textAlign: 'center', marginTop: 20}}>
-      <Typography use="caption" tag="h3">DevDocs Web Ext</Typography>
-      <Typography use="caption" tag="div">A Chrome Extension for DevDocs.</Typography>
-      <Typography use="caption" tag="div">version: 1.10.10 (3dasf4)</Typography>
+    <Elevation className='footer'>
+      <Typography use="caption" tag="h3">
+        DevDocs Web Ext
+        <span className='space'>-</span>
+        {i18n('optionsVersion')} 1.10.10
+      </Typography>
+      <Typography use="caption" tag="div">{i18n('optionsAboutDesc')}</Typography>
       <Typography use="caption" tag="div">
-        <a href="a">rate</a>
-        <span style={{padding: '0 5px'}}>·</span>
-        <a href="a">feedback</a>
-        <span style={{padding: '0 5px'}}>·</span>
-        <a href="a">source</a>
+        <a href="a" style={{color: 'inherit'}}>{i18n('optionsRate')}</a>
+        <span className='space'>·</span>
+        <a href="a" style={{color: 'inherit'}}>{i18n('optionsFeedback')}</a>
+        <span className='space'>·</span>
+        <a href="a" style={{color: 'inherit'}}>{i18n('optionsSource')}</a>
       </Typography>
     </Elevation>
+
+    <a className="github-fork-ribbon right-bottom fixed" href="https://github.com/arianrhodsandlot/DevDocs-Web-Ext" data-ribbon="Star me on GitHub" title="Star me on GitHub">Star me on GitHub</a>
   </>
 )
 
--- src/options/i18n.js
+++ src/options/i18n.js
@@ -1,5 +1,11 @@
 import browser from 'webextension-polyfill'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 export default function i18n (messageName) {
-  return browser.i18n.getMessage(messageName)
+  const message = browser.i18n.getMessage(messageName)
+  if (isDev && !message) {
+    console.warn(`Message for ${messageName} is empty!`)
+  }
+  return message
 }

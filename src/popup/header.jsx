f6f34d588c639b398688ed2a90d2fc13fea8d374
Arianrhod <theguidanceofawhitetower@gmail.com>
2019-03-05 18:26:34 +0800

    convert header to functional component(wip)

--- src/popup/header.jsx
+++ src/popup/header.jsx
@@ -1,43 +1,69 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import querystring from 'querystring'
 import { withRouter } from 'react-router'
 import key from 'keymaster'
 import browser from 'webextension-polyfill'
 
-class Header extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      doc: null,
-      inputPaddingLeft: 0,
-      ...this.getInputState()
-    }
-    if (this.state.scope) {
-      this.guessDocFromScope(this.state.scope)
-    }
-    this.inputRef = React.createRef()
-    this.scopeRef = React.createRef()
+export default withRouter(function Header ({location, history}) {
+  const [doc, setDoc] = useState(null)
+  const [inputPaddingLeft, setInputPaddingLeft] = useState(0)
+  const inputState = getInputState()
+  const [scope, setScope] = useState(inputState.scope)
+  const [query, setQuery] = useState(inputState.query)
+  const inputRef = useRef()
+  const scopeRef = useRef()
+  const keyHandlersRef = useRef()
+  keyHandlersRef.current = {guessDocFromScope}
 
-    this.handleChange = this.handleChange.bind(this)
-  }
+  useEffect(() => {
+    inputRef.current.select()
+    const clearQuery = false
+    guessDocFromScope(scope, clearQuery)
+    key('tab', () => {
+      if (doc) return false
+      const scope = inputRef.current.value.trim()
+      const clearQuery = true
+      keyHandlersRef.current.guessDocFromScope(scope, clearQuery)
+      return false
+    })
+    key('backspace', () => {
+      if (!inputRef.current.value) {
+        clearDoc()
+        return false
+      }
+    })
+    key('/', () => {
+      if (document.activeElement !== inputRef.current) {
+        inputRef.current.focus()
+        inputRef.current.select()
+        return false
+      }
+    })
+  }, [])
 
-  componentDidUpdate (prevProps, prevState) {
-    const {props, state} = this
-    if (props.location !== prevProps.location) {
-      this.setState(this.getInputState())
-    }
-    if (state.doc && state.doc !== prevState.doc) {
-      this.setState({inputPaddingLeft: this.scopeRef.current.offsetWidth + 10})
-    }
-    this.inputRef.current.focus()
-  }
+  useEffect(() => {
+    if (scopeRef.current) setInputPaddingLeft(scopeRef.current.offsetWidth + 10)
+    inputRef.current.focus()
+  }, [doc])
 
-  getInputState () {
-    const {props} = this
+  useEffect(() => {
+    const inputState = getInputState()
+    setScope(inputState.scope)
+    setQuery(inputState.query)
+    ;(async () => {
+      const doc = await browser.runtime.sendMessage({
+        action: 'auto-compelete-enabled-doc',
+        payload: { scope: inputState.scope }
+      })
+      setDoc(doc)
+    })()
+  }, [location])
+
+  function getInputState () {
     let query
     let scope
-    if (props.location.pathname === '/search') {
-      const parsed = querystring.parse(props.location.search.slice(1))
+    if (location.pathname === '/search') {
+      const parsed = querystring.parse(location.search.slice(1))
       query = parsed.query
       scope = parsed.scope
     }
@@ -46,80 +72,40 @@ class Header extends Component {
     return {query, scope}
   }
 
-  componentDidMount () {
-    this.inputRef.current.select()
-
-    key.filter = () => true
-    key('tab', () => {
-      if (!this.state.doc) {
-        const scope = this.inputRef.current.value.trim()
-        this.guessDocFromScope(scope, () => {
-          localStorage.scope = scope
-          localStorage.query = ''
-          this.inputRef.current.value = localStorage.query
-          this.props.history.replace('/search?query=' + encodeURIComponent(this.state.query) + '&scope=' + encodeURIComponent(this.state.scope))
-        })
-      }
-      return false
-    })
-    key('backspace', () => {
-      if (!this.inputRef.current.value) {
-        this.clearDoc()
-        return false
-      }
-    })
-    key('/', () => {
-      if (document.activeElement !== this.inputRef.current) {
-        this.inputRef.current.focus()
-        this.inputRef.current.select()
-        return false
-      }
-    })
-  }
-
-  async guessDocFromScope (scope, cb) {
+  async function guessDocFromScope (scope, clearQuery) {
     if (!scope) return
     const doc = await browser.runtime.sendMessage({
       action: 'auto-compelete-enabled-doc',
       payload: { scope }
     })
     if (doc) {
-      this.setState({doc, scope, query: ''}, () => {
-        if (cb) cb()
-      })
+      history.replace('/search?query=' + encodeURIComponent(clearQuery ? '' : query) + '&scope=' + encodeURIComponent(scope))
     }
   }
 
-  clearDoc () {
-    localStorage.scope = ''
-    this.setState({doc: null, scope: localStorage.scope, inputPaddingLeft: 0})
-    this.props.history.replace('/search?query=' + encodeURIComponent(this.state.query))
+  function clearDoc () {
+    history.replace('/search')
   }
 
-  handleChange () {
-    const query = this.inputRef.current.value.trim()
-    localStorage.query = query
-    this.props.history.replace('/search?query=' + encodeURIComponent(query) + '&scope=' + encodeURIComponent(this.state.scope))
+  function handleChange (e) {
+    e.persist()
+    const query = e.currentTarget.value
+    history.replace('/search?query=' + encodeURIComponent(query) + '&scope=' + encodeURIComponent(scope))
   }
 
-  render () {
-    const {query, doc, inputPaddingLeft} = this.state
-    return (
-      <div className='_header'>
-        <form className='_search' autoComplete='off'>
-          <svg><use href='#icon-search' /></svg>
-          <input defaultValue={query} placeholder='Search...' className='input _search-input' spellCheck='false' onChange={this.handleChange} autoFocus ref={this.inputRef} style={doc ? {paddingLeft: inputPaddingLeft} : {}} />
-          {doc ? <div className='_search-tag' ref={this.scopeRef}>{doc.fullName}</div> : null}
-        </form>
+  return (
+    <div className='_header'>
+      <form className='_search' autoComplete='off'>
+        <svg><use href='#icon-search' /></svg>
+        <input value={query} placeholder='Search...' className='input _search-input' spellCheck='false' onChange={handleChange} autoFocus ref={inputRef} style={doc ? {paddingLeft: inputPaddingLeft} : {}} />
+        {doc ? <div className='_search-tag' ref={scopeRef}>{doc.fullName}</div> : null}
+      </form>
 
-        <svg className='_settings' xmlns='http://www.w3.org/2000/svg'>
-          <defs>
-            <symbol id='icon-search' viewBox='0 0 24 24'><path d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z' /></symbol>
-          </defs>
-        </svg>
-      </div>
-    )
-  }
-}
-
-export default withRouter(Header)
+      <svg className='_settings' xmlns='http://www.w3.org/2000/svg'>
+        <defs>
+          <symbol id='icon-search' viewBox='0 0 24 24'><path d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z' /></symbol>
+        </defs>
+      </svg>
+    </div>
+  )
+})
--- src/popup/index.js
+++ src/popup/index.js
@@ -3,10 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import storage from '../common/storage'
 import App from './App.jsx'
+import key from 'keymaster'
 
 if (process.env.NODE_ENV === 'production') {
   Raven.config('https://d2ddb64170f34a2ca621de47235480bc@sentry.io/1196839').install()
 }
+
+key.filter = () => true
 
 async function main () {
   const {width, height, theme} = await storage.get()

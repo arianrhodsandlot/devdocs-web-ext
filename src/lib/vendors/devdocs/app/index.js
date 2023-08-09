16741ab317202ccaa22bbb58076313c87c24669e
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-08-09 17:48:11 +0800

    fix: fix tree shaking problem after upgrading parcel

--- src/lib/vendors/devdocs/app/index.js
+++ src/lib/vendors/devdocs/app/index.js
@@ -3,5 +3,8 @@ import { app } from './app'
 // these modules have no exports, instead they make side effects on the `app`
 import './searcher'
 import '../models/entry'
+console.assert(app.models.Entry)
+console.assert(app.Searcher)
+console.assert(app.SynchronousSearcher)
 
 export { app }

59e084f9d0a7f422d0339b64191e53b42b506792
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2023-08-30 11:07:45 +0800

    feat: update bundled assets

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import { createConfig } from '@arianrhodsandlot/eslint-config'
 
 export default createConfig({
-  typeChecking: false,
   overrides: {
     js: { ignores: ['src/lib/vendors/devdocs/**/*.js'] },
   },
